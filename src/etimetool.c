5a91d0b35895d396eee105557c85584308a7931d
Alexander V. Nikolaev <avn@daemon.hole.ru>
2009-07-14 10:49:38 +0000

    Skeleton

--- src/etimetool.c
+++ src/etimetool.c
@@ -1,8 +1,11 @@
+#define _GNU_SOURCE 1
 #include <stdio.h>
+#include <stdlib.h>
 
 void
 hwclock(int year, int month, int day, int h, int m)
 {
+    char *s;
 	asprintf(&s, "date %02d%02d%02d%02d20%02d; hwclock --systohc",
             month, day, h, m, year);
 	if(s)
